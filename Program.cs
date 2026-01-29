58e79a920077f37255bf7ecc9cbb1bab8d174970
Bernd Hagen <31480734+BerndHagen@users.noreply.github.com>
2026-01-29 19:28:39 +0100

    Version 1.1.3

---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace MetaEdit
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(mainForm);
+        }
+    }
+}
